5e0610a9f70057c41ce59af6116556237aafaeb8
hannayatsevich <hannayatsevich@gmail.com>
2020-09-13 15:52:08 +0300

    feat: add functions that are the solution to the subtasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr)) {    
+    let midArray = arr.map( value => {
+      return ( value === '--discard-next' || value === '--discard-prev' || value === '--double-next' || value === '--double-prev') ? 0 : 1;
+    });
+    arr.forEach( (element, idx) => {
+      switch(element) {        
+        case '--discard-next': 
+          if(arr[idx + 1] !== undefined && midArray[idx + 1]) midArray[idx + 1]--; 
+          break;
+        case '--discard-prev': 
+          if(arr[idx - 1] !== undefined && midArray[idx - 1]) midArray[idx - 1]--; 
+          break;
+        case '--double-next': 
+          if(arr[idx + 1] !== undefined && midArray[idx + 1]) midArray[idx + 1]++;
+          break;
+        case '--double-prev':
+          if(arr[idx - 1] !== undefined && midArray[idx - 1]) midArray[idx - 1]++;  
+          break;
+        default: break;
+      };
+    });
+    return midArray.reduce( (acc, value, idx) => {
+      if(value) {
+        for(let x = 1; x <= value; x++) {
+          acc.push(arr[idx])
+        };
+      };
+      return acc;
+    }, []);
+  }
+  else throw Error;
 };
